7ce97c877cbe548233d301c017940de002576240
victorgrytsunyak <93374084+victorgrytsunyak@users.noreply.github.com>
2022-05-13 15:25:23 +0300

    Script for centos

--- script.sh
+++ script.sh
@@ -11,7 +11,7 @@
       IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
       METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
 
-      sudo cat <<EOF > /var/www/html/index.html
+      sudo cat <<EOF > /usr/share/nginx/html/index.html
 <pre>
 Name: $NAME
 IP: $IP
